6a11c4493141897cd6ac8f8337fd295f73f74414
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-10-09 20:58:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,48 +4,70 @@ import asyncio
 import re
 import aiohttp
 import urllib
-import os
 from bs4 import BeautifulSoup
 
 bot = commands.Bot(command_prefix="!")
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
 @bot.event
 async def on_ready():
     print("開始")
     bot.ch = bot.get_channel(603442982057934854)
-    bot.session = aiohttp.ClientSession()
-    q.start()
+    bot.tao = bot.ch.guild.get_member(526620171658330112)
 
-@tasks.loop(seconds=15)
-async def q():
+    bot.session = aiohttp.ClientSession()
+    bot.task = bot.loop.create_task(quiz())
+
+    bot.q_count = 0
+    bot.s_count = 0
+
+async def quiz():
+    msg = ""
     
     def check(m):
-        return m.author.id==526620171658330112 and m.channel.id==603442982057934854 and m.embeds
+        return m.author == bot.tao and m.embeds[0].description
 
-    await bot.ch.send("::t")
-    msg = await bot.wait_for('message',check=check)
+    def end_check(m):
+        l = ["残念","正解","時間切れ"]
+        return m.author == bot.tao and any(i in m.embeds[0].description for i in l)
 
+    while not bot.is_closed():
+        await bot.ch.send("::t")
+        msg = await bot.wait_for('message',check=check)
 
-    s = msg.embeds[0].description
-    s = re.search("「(.*)」の読み方をひらがなで答えなさい。",s)
-        
-    if s:
+        while not msg.embeds[0].description.startswith("「"):
+            msg = await bot.wait_for('message',check=check)
+
+        s = msg.embeds[0].description
+        s = re.search("「(.*)」の読み方をひらがなで答えなさい。",s)
+
+        if s is None:
+            continue
+
         s = s.group(1)
         url = f"https://dictionary.goo.ne.jp/word/{urllib.parse.quote(s)}/"
         
         async with bot.session.get(url) as resp:
             text = await resp.text()
             parsed = BeautifulSoup(text, "html.parser")
-
-        result = re.search(".*\((.*)\)の意味・使い方 - 四字熟語一覧 - goo辞書",str(parsed.title))        
+            
+        result = re.search(".*\((.*)\)の意味・使い方 - 四字熟語一覧 - goo辞書",str(parsed.title))
         await asyncio.sleep(5)
 
         if result:
             await bot.ch.send(result.group(1))
         else:
             await bot.ch.send("わからない")
-        
+
+        ans_m = await bot.wait_for('message',check=end_check)
+        bot.q_count += 1
+
+        if ans_m.embeds[0].description.startswith("正解"):
+            bot.s_count += 1
+
+@bot.command()
+async def info(ctx):
+    await ctx.send(f"稼働開始からの問題数：{bot.q_count}\n正解数：{bot.s_count}")
+
 bot.run(token)
