2bc47c2a55c148d16121dfb40c82af21402b3487
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-10-09 18:58:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,29 +23,24 @@ async def on_ready():
 async def q():
     
     def check(m):
-        s = m.embeds[0].description
-        return m.author.id==526620171658330112 and (not s.startswith("時間切れ") and not s.startswith("正解だ") and not s.startswith("残念"))
+        return m.author.id==526620171658330112 and m.channel.id==603442982057934854 and m.embeds
 
     await bot.ch.send("::t")
     msg = await bot.wait_for('message',check=check)
 
-    if msg.embeds:
 
-        s = msg.embeds[0].description
-        s = re.search("「(.*)」の読み方をひらがなで答えなさい。",s)
+    s = msg.embeds[0].description
+    s = re.search("「(.*)」の読み方をひらがなで答えなさい。",s)
         
-        if not s:
-            return
+    if s:
         s = s.group(1)
-
         url = f"https://dictionary.goo.ne.jp/word/{urllib.parse.quote(s)}/"
         
         async with bot.session.get(url) as resp:
             text = await resp.text()
             parsed = BeautifulSoup(text, "html.parser")
 
-        result = re.search(".*\((.*)\)の意味・使い方 - 四字熟語一覧 - goo辞書",str(parsed.title))
-        
+        result = re.search(".*\((.*)\)の意味・使い方 - 四字熟語一覧 - goo辞書",str(parsed.title))        
         await asyncio.sleep(5)
 
         if result:
